cee6d2a95e986c1a1af1bfb134be3e22e6cbfad7
RonaldMartinsCruz <18647552709@cefet-rj.br>
2025-05-16 22:57:27 -0300

    ajustando conexao com banco para deploy

--- controller/conexao.php
+++ controller/conexao.php
@@ -2,10 +2,22 @@
 
 namespace controller;
 
+$databaseUrl = getenv('DATABASE_URL');
+
+if (!$databaseUrl) {
+    die('DATABASE_URL não está definida');
+}
+
+$parsed = parse_url($databaseUrl);
+$host = $parsed['host'];
+$user = $parsed['user'];
+$pass = $parsed['pass'];
+$dbname = ltrim($parsed['path'], '/');
+$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
 
 function criarPDO(){
     try{
-        $pdo = new \PDO("mysql:dbname=agenda;charset=utf8;host=localhost", 'root', '', 
+        $pdo = new \PDO($dsn, $user, $pass, 
         [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
         \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC]);
         return $pdo;
