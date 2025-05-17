551a7e0842d18c926b0718b8360388da6284933b
RonaldMartinsCruz <18647552709@cefet-rj.br>
2025-05-16 23:36:47 -0300

    ajustando deploy

--- controller/conexao.php
+++ controller/conexao.php
@@ -2,27 +2,28 @@
 
 namespace controller;
 
-$databaseUrl = getenv('DATABASE_URL');
+function criarPDO() {
+    $databaseUrl = getenv('DATABASE_URL');
 
-if (!$databaseUrl) {
-    die('DATABASE_URL não está definida');
-}
+    if (!$databaseUrl) {
+        die('DATABASE_URL não está definida');
+    }
 
-$parsed = parse_url($databaseUrl);
-$host = $parsed['host'];
-$user = $parsed['user'];
-$pass = $parsed['pass'];
-$dbname = ltrim($parsed['path'], '/');
-$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+    $parsed = parse_url($databaseUrl);
+    $host = $parsed['host'];
+    $user = $parsed['user'];
+    $pass = $parsed['pass'];
+    $dbname = ltrim($parsed['path'], '/');
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
 
-function criarPDO(){
-    try{
-        $pdo = new \PDO($dsn, $user, $pass, 
-        [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC]);
+    try {
+        $pdo = new \PDO($dsn, $user, $pass, [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
+        ]);
         return $pdo;
-    }catch(Exception $e){
-        die("Não foi possível se conectar com o banco de dados");
+    } catch (\Exception $e) {
+        die("Não foi possível se conectar com o banco de dados: " . $e->getMessage());
     }
 }
 
